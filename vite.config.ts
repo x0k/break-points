20286de57cdb3c9d27cc7643efe42b4364188f4c
Krasilnikov Roman <x0kill@ya.ru>
2023-09-01 21:53:55 +0300

    Install basic ssl plugin for vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,19 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 // import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/break-points/',
-  plugins: [svelte()],
+  // server: { https: true },
+  plugins: [
+    basicSsl(),
+    svelte()
+  ],
   resolve: {
     alias: {
-      '@': path.resolve('./src')
-    }
-  }
+      '@': path.resolve('./src'),
+    },
+  },
 })
