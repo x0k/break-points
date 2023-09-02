88e02364b1e8ed355c5fb5f50a67fd99da7677ec
Krasilnikov Roman <x0kill@ya.ru>
2023-09-02 22:18:44 +0300

    Setup for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,38 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-// import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/break-points/',
-  // server: { https: true },
   plugins: [
     basicSsl(),
-    svelte()
+    svelte(),
+    VitePWA({
+      manifest: {
+        name: 'Break Points',
+        description: 'App for storing your route points',
+        icons: [
+          { src: '/favicon.ico', type: 'image/x-icon', sizes: '16x16 32x32' },
+          { src: '/icon-192.png', type: 'image/png', sizes: '192x192' },
+          { src: '/icon-512.png', type: 'image/png', sizes: '512x512' },
+          {
+            src: '/icon-192-maskable.png',
+            type: 'image/png',
+            sizes: '192x192',
+            purpose: 'maskable',
+          },
+          {
+            src: '/icon-512-maskable.png',
+            type: 'image/png',
+            sizes: '512x512',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
