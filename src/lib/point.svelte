8ecf331dc952364569d7ecf54651b39f36fd9768
Krasilnikov Roman <x0kill@ya.ru>
2023-12-12 02:39:33 +0300

    Point component

--- src/lib/point.svelte
+++ src/lib/point.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
   import { MapPin } from 'lucide-svelte'
   export let title: string
+  // on:click used to bubble up event to parent
 </script>
 
-<div class="flex gap-2 align-baseline" on:click>
+<button class="flex gap-2 align-baseline" on:click>
   <slot name="prepend" />
   <MapPin class="min-w-[1.5rem]" />
   <span class="truncate shrink-0 max-w-[40%]">{title}</span>
   <slot name="append" />
-</div>
+</button>
