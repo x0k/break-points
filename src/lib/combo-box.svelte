82599b41d88f20b5cbaade6e741b8942e5c294ba
Krasilnikov Roman <x0kill@ya.ru>
2023-12-12 15:34:20 +0300

    Combo box async filter

--- src/lib/combo-box.svelte
+++ src/lib/combo-box.svelte
@@ -7,13 +7,13 @@
   import { Button } from '@/lib/components/button'
   import { cn } from '@/lib/utils'
 
-  export let items: T[]
+  export let items: T[] = []
   export let selected: T | null
   export let defaultLabel = 'Select an item'
   export let searchPlaceholder = 'Search items'
   export let getItemLabel: (item: T) => string
   export let getItemId: (item: T) => string
-  export let filter: (items: T[], search: string) => T[]
+  export let filter: (items: T[], search: string) => T[] | Promise<T[]>
   export let onSelect: (item: T) => void
   let className = ''
   export { className as class }
@@ -45,36 +45,43 @@
       variant="outline"
       role="combobox"
       aria-expanded={open}
-      class={cn("w-[200px] justify-between", className)}
+      class={cn('w-[200px] justify-between', className)}
     >
-      {selectedItemLabel}
-      <ChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
+      <span class="truncate">
+        {selectedItemLabel}
+      </span>
+      <ChevronsUpDown class="ml-auto h-4 w-4 shrink-0 opacity-50" />
     </Button>
   </Popover.Trigger>
-  <Popover.Content class={cn("w-[200px] p-0", contentClass)}>
+  <Popover.Content class={cn('w-[200px] p-0', contentClass)}>
     <Command.Root shouldFilter={false}>
       <Command.Input placeholder={searchPlaceholder} bind:value={search} />
+      <Command.Empty>
+        <slot name="no-results">Nothing found</slot>
+      </Command.Empty>
       <Command.Group>
-        {#each filtered as item, _ (getItemId(item))}
-          {@const id = getItemId(item)}
-          <Command.Item
-            value={id}
-            onSelect={() => {
-              onSelect(item)
-              closeAndFocusTrigger(ids.trigger)
-            }}
-          >
-            <Check
-              class={cn(
-                'mr-2 h-4 w-4',
-                selectedItemId !== id && 'text-transparent'
-              )}
-            />
-            {getItemLabel(item)}
-          </Command.Item>
-        {:else}
-          <slot name="no-results">Nothing found</slot>
-        {/each}
+        {#await filtered}
+          <Command.Item disabled>Loading...</Command.Item>
+        {:then items}
+          {#each items as item, _ (getItemId(item))}
+            {@const id = getItemId(item)}
+            <Command.Item
+              value={id}
+              onSelect={() => {
+                onSelect(item)
+                closeAndFocusTrigger(ids.trigger)
+              }}
+            >
+              <Check
+                class={cn(
+                  'mr-2 h-4 w-4',
+                  selectedItemId !== id && 'text-transparent'
+                )}
+              />
+              {getItemLabel(item)}
+            </Command.Item>
+          {/each}
+        {/await}
       </Command.Group>
     </Command.Root>
   </Popover.Content>
